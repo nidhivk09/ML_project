ab37e6b84b50f282f1efce084b31cf0422057ca2
nidhivk09 <nidhivk09@gmail.com>
2024-10-05 13:45:53 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
 
 st.title('Machine Learning App')
 st.info('This is a machine learning app.')
@@ -10,13 +11,13 @@ with st.expander('Data'):
   df
 
   st.write('**X**')
-  X=df.drop('species',axis=1)
-  X
+  X_raw=df.drop('species',axis=1)
+  X_raw
   
   
   st.write('**Y**')
-  Y=df.species
-  Y
+  Y_raw=df.species
+  Y_raw
 
 
 with st.expander('Data visualization'):
@@ -38,19 +39,57 @@ with st.sidebar:
   data={'island':island,'bill_length_mm':bill_length_mm,'sex':gender,'bill_depth_mm':bill_depth_mm, 'flipper_length_mm':flipper_length_mm,'body_mass_g':body_mass_g}
 
   input_df=pd.DataFrame(data,index=[0])
-  input_penguins=pd.concat([input_df,X],axis=0)
+  input_penguins=pd.concat([input_df,X_raw],axis=0)
 
-
-with st.expander('Input Features'):
-  st.write('**Input penguin**')
-  input_df
-  st.write('**Combined penquins data**')
-  input_penguins
-
-
-#Encode
-
+  
+  with st.expander('Input Features'):
+    st.write('**Input penguin**')
+    input_df
+    st.write('**Combined penquins data**')
+    input_penguins
+  #Encode
+#Encode X
 encode=['island','sex']
 df_penguins=pd.get_dummies(input_penguins,prefix=encode)
+X=df_penguins[1:]
+input_row=df_penguins[:1]
+
+#encode Y
+taregt_mapper={'Adelie':0,'Chinstrap':1,'Gentoo':2}
+
+def target_encode(val):
+  return target_mapper[val]
+
+y=Y_raw.apply(target_encode)
+y
+
+
+
+
+with st.exapnder('Data Expander'):
+  
+
+  st.write('**Encoded X (input penguin)**')
+  input_row
+  st-write('**Encoded Y**')
+  y
+
+#model training and inference
+
+clf=RandomForestClassifier()
+clf.fit(X,y)
+
+# apply model to make predictions
+
+prediction=clf.prediction(input_row)
+prediction_proba=clf.predict_proba(input_row)
+
+df_prediction_proba=pd.DataFrame(prediction_proba)
+df_prediction_proba.columns=['Adelie','Chinstrap','Gentoo']
+df_prediction_proba.rename(columns={0:'Adelie',1:'Chinstrap','2:'Gentoo'})
+df_prediction_proba
+
+
+
                               
                                 
