4387e501bd55890f88f0b57a8f6fa573ab438e51
nidhivk09 <nidhivk09@gmail.com>
2024-10-05 13:54:10 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -94,6 +94,8 @@ df_prediction_proba
 
 st.subheader('Predicted Species')
 
+df_prediction_proba
+
 penguins_species=np.array(['Adelie','Chinstrap','Gentoo'])
 st.success(str(penguins_species[prediction][0]))
 
