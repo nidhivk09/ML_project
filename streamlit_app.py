2183d1906fb5d7d004157b6b30d7ba59c1b1d1f4
nidhivk09 <nidhivk09@gmail.com>
2024-10-05 09:38:13 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,17 @@ import pandas as pd
 
 st.title('Machine Learning App')
 st.info('This is a machine learning app.')
-st.write('Hello world!')
 
-df=pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/refs/heads/master/penguins_cleaned.csv')
-df
+with st.expander('Data'):
+  st.write('**Raw data**')
+  df=pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/refs/heads/master/penguins_cleaned.csv')
+  df
+
+st.write('**X**')
+X=df.drop('species',axis=1)
+X
+
+
+st.write('**Y**')
+Y=df.species
+Y
