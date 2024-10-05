63f8d00cb81cec045b1f9432c80a6aa089c855ac
nidhivk09 <nidhivk09@gmail.com>
2024-10-05 14:11:52 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,10 +37,10 @@ with st.sidebar:
 
 # create dataframe for input features
 
-  data={'island':island,'bill_length_mm':bill_length_mm,'sex':gender,'bill_depth_mm':bill_depth_mm, 'flipper_length_mm':flipper_length_mm,'body_mass_g':body_mass_g}
+data={'island':island,'bill_length_mm':bill_length_mm,'sex':gender,'bill_depth_mm':bill_depth_mm, 'flipper_length_mm':flipper_length_mm,'body_mass_g':body_mass_g}
 
-  input_df=pd.DataFrame(data,index=[0])
-  input_penguins=pd.concat([input_df,X_raw],axis=0)
+input_df=pd.DataFrame(data,index=[0])
+input_penguins=pd.concat([input_df,X_raw],axis=0)
 
   
   with st.expander('Input Features'):
